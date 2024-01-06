ccc76575adbdd1ea6192e793eaef478a5194d6c4
NahuelAlbertoRivas <nahuelrivas41@gmail.com>
2024-01-05 23:09:15 -0300

    updated 5-1-24

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,34 @@
+// Este archivo siempre será renderizado primero
+import type { LinksFunction } from "@remix-run/node";
+
 import {
+  Outlet, // En primer instancia, este componente se utiliza para renderizar la ruta "hija" correspondiente a una ruta "padre"
   Form,
   Links,
   LiveReload,
   Meta,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  json,
+  Link,
 } from "@remix-run/react";
 
+import appStylesHref from "./app.css";
+import { getContacts } from "./data";
+import Contact from "./routes/contacts.$contactId";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export async function loader() {  // importamos la fn. "loader" a fin de poder llamar al método getContacts()
+  const contacts = await getContacts(); // la fn. "getContacts()" genera un array con datos ficticios para los fines de esta práctica; nótese que se emula el acceso o bien una consulta a una API/db
+  return json(contacts) // la fn. "json()" permite retornar el dato como una "response" con los headers adecuados dentro
+}
+
 export default function App() {
+  const contacts = useLoaderData<typeof loader>(); // Se guarda el array con los datos ficticios a ser iterados 
   return (
     <html lang="en">
       <head>
@@ -18,32 +39,52 @@ export default function App() {
       </head>
       <body>
         <div id="sidebar">
-          <h1>Remix Contacts</h1>
+          <h1>Contacts</h1>
           <div>
             <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
-                placeholder="Search"
+                placeholder="Search contact"
                 type="search"
                 name="q"
               />
               <div id="search-spinner" aria-hidden hidden={true} />
             </Form>
             <Form method="post">
-              <button type="submit">New</button>
+              <button type="submit">Add</button>
             </Form>
           </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+          <nav> 
+            {contacts.length ? ( // Se reemplaza la lista estática original propuesta, por la siguiente dinámica, finalmente se lleva a cabo el mapeo con cada dato ficticio recuperado
+                <ul>             
+                  {contacts.map((contact) => ( // obs: a partir de la línea que contiene la etiqueta "Link" se renderiza cada url asociada (de ahí el hecho que cada vez que clickeamos en un contacto diferente, la dirección se actualiza y aparecerá ' https://.../contacts/steve ', ' https://.../contacts/dorian ', etc)
+                    <li key={contact.id}> 
+                      <Link to={`contacts/${contact.id}`}> 
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? (
+                          <span>★</span>
+                        ) : null}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>
+                  <i>No contacts</i>
+                </p>
+              )} 
+          </nav> 
+        </div>
+
+        <div id="detail">
+          <Outlet/>
         </div>
 
         <ScrollRestoration />
---
+++ app/routes/contacts.$contactId.tsx
@@ -0,0 +1,113 @@
+import { Form, useLoaderData } from "@remix-run/react";
+
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+
+import type { FunctionComponent } from "react";
+
+import type { ContactRecord } from "../data";
+
+import { getContact } from "../data";
+import invariant from "tiny-invariant";
+
+export async function loader({ params } : LoaderFunctionArgs) {
+    invariant(params.contactId, "Missing contactId param"); // Se instala el pack 'tiny-invariant' para facilitar el chequeo en caso de que el 'contactId' no exista, si no hacemos esta verificación, al llamar getContact() lanzaría un error
+    const contactId = params.contactId;
+    const contact = await getContact(contactId);
+    if(!contact){ // Podría ser que no tengamos ningún contacto, entonces en esta línea verificamos eso
+        throw new Response("Not found", { status: 404 }); // En caso de que no existan contactos, lanzamos un error 
+    }
+    return json(contact); // Si existe, retornaremos el contacto a la siguiente fn. 'Contact()', justamente en la declaración ' const contact = useLoaderData<typeof loader>(); '
+  }
+
+export default function Contact() {
+  /* const contact = { // Este bloque es meramente de prueba, lo usamos al incio del desarrollo
+    first: "Your",     // para dar forma a lo que vamos creando
+    last: "Name",
+    avatar: "https://placekitten.com/g/200/200",
+    twitter: "your_handle",
+    notes: "Some notes",
+    favorite: true,
+  }; */
+
+  const contact = useLoaderData<typeof loader>(); // Recibimos el contacto existente
+
+  return (
+    <div id="contact">
+      <div>
+        <img
+          alt={`${contact.first} ${contact.last} avatar`}
+          key={contact.avatar}
+          src={contact.avatar}
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter ? (
+          <p>
+            <a
+              href={`https://twitter.com/${contact.twitter}`}
+            >
+              {contact.twitter}
+            </a>
+          </p>
+        ) : null}
+
+        {contact.notes ? <p>{contact.notes}</p> : null}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+
+          <Form
+            action="destroy"
+            method="post"
+            onSubmit={(event) => {
+              const response = confirm(
+                "Please confirm you want to delete this record."
+              );
+              if (!response) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Favorite: FunctionComponent<{
+  contact: Pick<ContactRecord, "favorite">;
+}> = ({ contact }) => {
+  const favorite = contact.favorite;
+
+  return (
+    <Form method="post">
+      <button
+        aria-label={
+          favorite
+            ? "Remove from favorites"
+            : "Add to favorites"
+        }
+        name="favorite"
+        value={favorite ? "false" : "true"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </Form>
+  );
+};
